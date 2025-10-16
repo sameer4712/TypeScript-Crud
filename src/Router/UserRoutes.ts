cb41cf3430b7dcb203911b68d6be97818a7f59e9
sameer4712 <sameerptmc@gmail.com>
2025-10-16 16:47:42 +0530

    finished

---
+++ src/Router/UserRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express"
+import { AddUser,GetAllUser,updateUser,deleteUser } from "../controller/UserController.ts"
+
+const router = Router()
+router.get('/allusers',GetAllUser)
+router.post("/addusers", AddUser)
+router.put('/edituser/:id',updateUser)
+router.delete('/deleteuser/:id',deleteUser)
+
+export default router
