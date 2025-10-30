272d7bf7bf06ce0511e5429afd682f5968f33a2b
sameer4712 <sameerptmc@gmail.com>
2025-10-30 18:56:26 +0530

    4th

--- src/Router/UserRoutes.ts
+++ src/Router/UserRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 import { AddUser,GetAllUser,updateUser,deleteUser } from "../controller/UserController.ts"
 
 const router = Router()
+
 router.get('/allusers',GetAllUser)
 router.post("/addusers", AddUser)
 router.put('/edituser/:id',updateUser)
