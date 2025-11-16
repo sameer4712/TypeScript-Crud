61fa15976f16db48a3bdf4b7f548434ff8340fea
sameer4712 <sameerptmc@gmail.com>
2025-11-16 22:56:05 +0530

    1st

--- examplle.ts
+++ examplle.ts
@@ -1,18 +1,15 @@
-class Name{
-    getinfo(name:string):string;
-    getinfo(id:number):string;
-    getinfo(value:any):string{
-        if(typeof value ==="number")
-        {
-            return `userid is ${value}`
-        }
-        else
-            {
-            return `username is ${value}`
-        }
+class Name {
+  getinfo(name: string): string;
+  getinfo(id: number): string;
+  getinfo(value: any): string {
+    if (typeof value === "number") {
+      return `userid is ${value}`;
+    } else {
+      return `username is ${value}`;
     }
+  }
 }
 
-const obj = new Name()
+const obj = new Name();
 console.log(obj.getinfo(100));
 console.log(obj.getinfo("sameer"));
