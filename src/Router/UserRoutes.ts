2af1c1c9774f3a4b86c3e924c30102c24080cedd
sameer4712 <sameerptmc@gmail.com>
2025-11-16 22:56:46 +0530

    5th

--- src/Router/UserRoutes.ts
+++ src/Router/UserRoutes.ts
@@ -1,13 +1,16 @@
-import { Router } from "express"
-import { AddUser,GetAllUser,updateUser,deleteUser } from "../controller/UserController.ts"
+import { Router } from "express";
+import {
+  AddUser,
+  GetAllUser,
+  updateUser,
+  deleteUser,
+} from "../controller/UserController.ts";
 
-const router = Router()
+const router = Router();
 
-router.get('/allusers',GetAllUser)
-router.post("/addusers", AddUser)
-router.put('/edituser/:id',updateUser)
-router.delete('/deleteuser/:id',deleteUser)
+router.get("/allusers", GetAllUser);
+router.post("/addusers", AddUser);
+router.put("/edituser/:id", updateUser);
+router.delete("/deleteuser/:id", deleteUser);
 
-
-
-export default router
+export default router;
