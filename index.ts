6050dfa832dfe5343997fbb70269c946c72c31a6
sameer4712 <sameerptmc@gmail.com>
2025-11-16 22:56:23 +0530

    2nd

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
-import  express  from "express";
+import express from "express";
 import mongoose from "mongoose";
-import router from "./src/Router/UserRoutes.ts"
-const app = express()
+import router from "./src/Router/UserRoutes.ts";
+const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
-}  
-)
+  res.send("Hello World");
+});
 const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/testdb", {
@@ -19,8 +18,8 @@ const connectDB = async () => {
 };
 connectDB();
 
-app.use(express.json())
-app.use("/",router)
+app.use(express.json());
+app.use("/", router);
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
