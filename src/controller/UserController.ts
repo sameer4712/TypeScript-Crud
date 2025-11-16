fa734a89551a918d5c8d2eebe6d3d09998a51468
sameer4712 <sameerptmc@gmail.com>
2025-11-16 22:56:29 +0530

    3rd

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,9 +1,7 @@
 import type { Request, Response } from "express";
 import usermodel from "../Model/User.ts";
 
-
 export const AddUser = async (req: Request, res: Response) => {
-  
   try {
     const { name, email, age } = req.body;
 
@@ -19,11 +17,10 @@ export const AddUser = async (req: Request, res: Response) => {
   }
 };
 
-
 export const GetAllUser = async (req: Request, res: Response) => {
   try {
     const users = await usermodel.find();
-    res.json({message:"All users listed",users});
+    res.json({ message: "All users listed", users });
   } catch (err) {
     console.error("Error fetching users:", err);
     res.status(500).json({ message: "Server error" });
@@ -38,31 +35,30 @@ export const updateUser = async (req: Request, res: Response) => {
     const updatedUser = await usermodel.findByIdAndUpdate(
       id,
       { name, email, age },
-      { new: true } 
+      { new: true }
     );
 
     if (!updatedUser)
       return res.status(404).json({ message: "User not found" });
 
-    res.json({message:`Updated user is ${updateUser}`});
+    res.json({ message: `Updated user is ${updateUser}` });
   } catch (err) {
     console.error("Error updating user:", err);
     res.status(500).json({ message: "Server error" });
   }
 };
 
-
 export const deleteUser = async (req: Request, res: Response) => {
   try {
-
     const { id } = req.params;
 
     const deleteuser = await usermodel.findByIdAndDelete(id);
 
-    if (!deleteuser)
-      return res.status(404).json({ message: "User not found" });
+    if (!deleteuser) return res.status(404).json({ message: "User not found" });
 
-    res.json({ message: `User deleted successfully : username: ${deleteuser.name}` });
+    res.json({
+      message: `User deleted successfully : username: ${deleteuser.name}`,
+    });
   } catch (err) {
     console.error("Error deleting user:", err);
     res.status(500).json({ message: "Server error" });
